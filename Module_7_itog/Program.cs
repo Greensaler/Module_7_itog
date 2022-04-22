b3feb705ab03db2a882385a731937b5b94fdee38
Greensaler <medvedev@topengineer.ru>
2022-04-22 23:23:36 +0300

    первый этап

--- Module_7_itog/Program.cs
+++ Module_7_itog/Program.cs
@@ -9,4 +9,41 @@ namespace Module_7_itog
             Console.WriteLine("Hello World!");
         }
     }
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery,
+    TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        // ... Другие поля
+    }
 }
+
